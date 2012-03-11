068b42ad687972fbfe937b6a1c8191e68d932d30
Sergei Lebedev <superbobry@gmail.com>
2012-03-11 14:21:32 +0400

    Added 'z_bin_to_pos_int'

--- src/z_validate.erl
+++ src/z_validate.erl
@@ -4,6 +4,7 @@
          z_proplist_get/2, z_proplist_get/3,
          z_int_in_range/2, z_int_in_range/3,
          z_bin_to_int/1, z_bin_to_int/2,
+         z_bin_to_pos_int/1, z_bin_to_pos_int/2,
          z_bin_to_list/1, z_bin_to_list/2,
          z_bin_to_bool/1, z_bin_to_bool/2,
          z_bin_to_ex_atom/1, z_bin_to_ex_atom/2,
@@ -99,6 +100,14 @@ z_bin_to_int(?Z_VALUE(_Val, Err)=ZVal) ->
 -spec z_bin_to_int(z_value(binary()), error_term()) -> z_value(integer()).
 z_bin_to_int(?Z_VALUE(Val, _Err), NewErr) ->
     ?Z_TRANSFORM(list_to_integer(binary_to_list(Val)), is_integer, NewErr).
+
+-spec z_bin_to_pos_int(z_value(binary())) -> z_value(pos_integer()).
+z_bin_to_pos_int(?Z_VALUE(_Val, Err)=ZVal) ->
+    z_bin_to_pos_int(ZVal, Err).
+
+-spec z_bin_to_pos_int(z_value(binary()), error_term()) -> z_value(pos_integer()).
+z_bin_to_pos_int(ZVal, NewErr) ->
+    z_verify(fun (X) -> X >= 0 end, z_bin_to_int(ZVal, NewErr)).
 
 -spec z_bin_to_list(z_value(binary())) -> z_value(list()).
 z_bin_to_list(?Z_VALUE(_Val, Err)=ZVal) ->
