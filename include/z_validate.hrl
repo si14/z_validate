207d9344e6c6f1dfb8a861846866e4e261f25039
Sergei Lebedev <superbobry@gmail.com>
2012-03-11 14:34:23 +0400

    Added 'z_bin_to_pos_int' to 'z_validate.hrl'

--- include/z_validate.hrl
+++ include/z_validate.hrl
@@ -13,6 +13,7 @@
                      z_proplist_get/2, z_proplist_get/3,
                      z_int_in_range/2, z_int_in_range/3,
                      z_bin_to_int/1, z_bin_to_int/2,
+                     z_bin_to_pos_int/1, z_bin_to_pos_int/2,
                      z_bin_to_list/1, z_bin_to_list/2,
                      z_bin_to_bool/1, z_bin_to_bool/2,
                      z_bin_to_ex_atom/1, z_bin_to_ex_atom/2,
